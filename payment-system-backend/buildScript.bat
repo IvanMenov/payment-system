133663ac8b6245fa50cc2034b70fe6af1529184c
Ivan Menov <vanetomenov@gmail.com>
2024-05-03 11:04:13 +0300

    enhance the buildScript

--- payment-system-backend/buildScript.bat
+++ payment-system-backend/buildScript.bat
@@ -1,3 +1,8 @@
 @echo off  
+IF /i "%1" == "with-test" GOTO :runWithTest
+GOTO :runWithoutTest
+:runWithoutTest
 ./gradlew spotlessApply clean build -x test
+:runWithTest
+./gradlew spotlessApply clean build
 exit /b
